59e1ef15fab03eb4548177823dab6adcf916ff7d
Jeeva4000 <vjeeva@gmail.com>
2023-11-15 13:45:50 +0530

    img

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './about.css';
-import ME from '../../assests/MERN-Stack-Development.png';
+import ME from '../../assests/MEAN-Stack-Work.gif';
 import { FaAward } from 'react-icons/fa';
 import { VscProject } from 'react-icons/vsc';
 import { LiaCertificateSolid } from 'react-icons/lia';
