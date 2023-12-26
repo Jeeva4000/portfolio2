e00bc8039aa55b9e8d6ffe91ce3490ecfb561f9a
Jeeva4000 <vjeeva@gmail.com>
2023-12-26 22:04:29 +0530

    nma

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 
         <h5>Hello I'm</h5>
         <h1>Jeeva</h1>
-        <h3 className='text-light'>Fullstack Web Developer</h3>
+        <h2 className='text-light'>Fullstack Web Developer</h2>
         <CTA />
         <HeaderSocials />
 
