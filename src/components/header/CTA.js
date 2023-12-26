bde4c0e7856af501f5061121275e6830c115441f
Jeeva4000 <vjeeva@gmail.com>
2023-12-26 12:03:07 +0530

    resume

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -5,7 +5,8 @@ const CTA = () => {
   return (
     <div className='cta'>
       {/* <a href={CV} download className='btn'>Resume</a> */}
-      <a href='https://drive.google.com/file/d/1IUeCngMxk27QdMR4fWJ2nSDmu8Bt9Z8J/view?usp=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a>
+      {/* <a href='https://drive.google.com/file/d/1IUeCngMxk27QdMR4fWJ2nSDmu8Bt9Z8J/view?usp=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a> */}
+      <a href='https://drive.google.com/file/d/13U4vfzdrCZy0rvMQfSIOzclgpCcuw5UN/view?usp=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
 
     </div>
