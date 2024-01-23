c38366f1230634eb2143768c8adbf7d89cfebf17
Jeeva4000 <vjeeva@gmail.com>
2024-01-23 22:04:25 +0530

    small changes

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -20,18 +20,19 @@ const Projects = () => {
             <img src={IMG1} alt='' />
           </div>
           <h3>Web Scrapping</h3>
-          <h4>
+          <h5>
             Web Scraping project that involves both frontend and backend components.
             The frontend provides a user interface, while the backend handles the scraping logic.
-          </h4>
+          </h5>
 
-          <h2>
-            ⋄ MongoDB
+          <h4 className='projects__item-h4'>
+
             ⋄ React
+            ⋄ MongoDB
             ⋄ Express
             ⋄ nodejs
 
-          </h2>
+          </h4>
           <div className='projects__item-cta'>
             <a href='https://github.com/Jeeva4000/webscrapperbackend' className='btn' target='_blank' rel="noreferrer">Frontend</a>
             <a href='https://github.com/Jeeva4000/webscrappingfrontends' className='btn' target='_blank' rel="noreferrer">Backend</a>
@@ -43,6 +44,18 @@ const Projects = () => {
             <img src={IMG2} alt='' />
           </div>
           <h3>Item Catalog</h3>
+          <h5>
+            The Item Catalog project is a web application designed for managing and cataloging items. Users can interact with the application to view, add, update, and delete items within the catalog.
+          </h5>
+
+          <h4 className='projects__item-h4'>
+
+            ⋄ React
+            ⋄ MongoDB
+            ⋄ Express
+            ⋄ nodejs
+
+          </h4>
           <div className='projects__item-cta'>
             <a href='https://github.com/Jeeva4000/capstonefrontend' className='btn' target='_blank' rel="noreferrer">Frontend</a>
             <a href='https://github.com/Jeeva4000/capstonebackend1' className='btn' target='_blank' rel="noreferrer">Backend</a>
@@ -54,6 +67,18 @@ const Projects = () => {
             <img src={IMG3} alt='' />
           </div>
           <h3>Interview Notes</h3>
+          <h5>
+            The Interview Notes project is a web application designed to assist users in organizing and managing notes related to job interviews. It provides a platform for users to keep track of interview details, questions, and other relevant information.
+          </h5>
+
+          <h4 className='projects__item-h4'>
+
+            ⋄ React
+            ⋄ MongoDB
+            ⋄ Express
+            ⋄ nodejs
+
+          </h4>
           <div className='projects__item-cta'>
             <a href='https://github.com/Jeeva4000/interview' className='btn' target='_blank' rel="noreferrer">Frontend</a>
             <a href='https://github.com/Jeeva4000/interview-notes-server' className='btn' target='_blank' rel="noreferrer">Backend</a>
@@ -65,6 +90,18 @@ const Projects = () => {
             <img src={IMG4} alt='' />
           </div>
           <h3>Notes App</h3>
+          <h5>
+            The Notes App is a web application designed to help users create, manage, and organize their notes. It provides a digital platform for users to jot down thoughts, ideas, to-do lists, or any other information they want to remember.
+          </h5>
+
+          <h4 className='projects__item-h4'>
+
+            ⋄ React
+            ⋄ MongoDB
+            ⋄ Express
+            ⋄ nodejs
+
+          </h4>
           <div className='projects__item-cta'>
             <a href='https://github.com/Jeeva4000/notefe' className='btn' target='_blank' rel="noreferrer">Frontend</a>
             <a href='https://github.com/Jeeva4000/notesr' className='btn' target='_blank' rel="noreferrer">Backend</a>
@@ -75,7 +112,19 @@ const Projects = () => {
           <div className='projects__item-image'>
             <img src={IMG5} alt='' />
           </div>
-          <h3>CRUD</h3>
+          <h3>Data Management App</h3>
+          <h5>
+            The Data Management CRUD App is a web application designed to provide users with the ability to manage data efficiently. It allows users to create new records, view existing data, update records, and delete entries from a dataset.
+          </h5>
+
+          <h4 className='projects__item-h4'>
+
+            ⋄ React
+            ⋄ MongoDB
+            ⋄ Express
+            ⋄ nodejs
+
+          </h4>
           <div className='projects__item-cta'>
             <a href='https://github.com/Jeeva4000/crudclient' className='btn' target='_blank' rel="noreferrer">Frontend</a>
             <a href='https://github.com/Jeeva4000/crud-serverr' className='btn' target='_blank' rel="noreferrer">Backend</a>
